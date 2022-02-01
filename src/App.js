e499878fc33c64eb74f76c30e676c31e2b6cb79a
thomass1510 <91555445+thomass1510@users.noreply.github.com>
2022-01-31 22:32:10 -0300

    adding navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/Navbar/Navbar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
